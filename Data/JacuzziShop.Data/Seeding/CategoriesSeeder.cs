3d09066bd7869c0f052e794800ee21a84c620dae
admin <stefanivanovc@gmail.com>
2022-10-25 20:16:24 +0300

    Add two seeders for Extras and Categories

---
+++ Data/JacuzziShop.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,29 @@
+﻿namespace JacuzziShop.Data.Seeding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using JacuzziShop.Data.Models;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            await dbContext.Categories.AddAsync(new Category { Name = "Luxyry" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Outdoor" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Indor" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Premium" });
+            await dbContext.Categories.AddAsync(new Category { Name = "UltraCheap" });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Data/JacuzziShop.Data/Seeding/ExtrasSeeder.cs
@@ -0,0 +1,25 @@
+﻿namespace JacuzziShop.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using JacuzziShop.Data.Models;
+
+    public class ExtrasSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Extras.Any())
+            {
+                return;
+            }
+
+            await dbContext.Extras.AddAsync(new Extra { Name = "Ultra bubbles" });
+            await dbContext.Extras.AddAsync(new Extra { Name = "Turbo jets" });
+            await dbContext.Extras.AddAsync(new Extra { Name = "Waterfall" });
+            await dbContext.Extras.AddAsync(new Extra { Name = "Underwater music" });
+            await dbContext.Extras.AddAsync(new Extra { Name = "Romantic lightening" });
+        }
+    }
+}
